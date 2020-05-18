b199d837698977e714ae842a6005302bca0988be
Federico Abad <federicoabad13@gmail.com>
2020-05-18 17:52:39 -0300

    Cree la aplacación Usuario con el model Perfil

--- api/api/settings.py
+++ api/api/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'usuario'
 ]
 
 ROOT_URLCONF = 'api.urls'
--- api/usuario/models.py
+++ api/usuario/models.py
@@ -1,6 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import (BaseUserManager, AbstractUser)
+from django.contrib.auth import get_user_model
 
 
-class Usuario(models.Model):
-    usuario = 
+class Perfil(models.Model):
+    class Meta:
+        verbose_name = 'Perfil'
+        verbose_name_plural = 'Perfiles'
+
+    usuario = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, blank=True, null=True)
+    telefono = models.CharField(max_length=25)
+    domicilio = models.CharField(max_length=100)
+    referencia = models.TextField(blank=True, help_text='Cómo nos conociste?')
+
+    def __str__(self):
+        return 'Perfil del usuario: %s' % self.usuario.get_full_name()
